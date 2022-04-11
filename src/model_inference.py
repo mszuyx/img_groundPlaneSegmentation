538ecfdac6c068c0145bacabbf5e6a71aeda4173
mszuyx <37651144+mszuyx@users.noreply.github.com>
2022-04-11 18:33:47 -0500

    Add files via upload
    Implement the new perlin model

--- src/model_inference.py
+++ src/model_inference.py
@@ -9,7 +9,7 @@ from openvino.inference_engine import IECore
 
 class Img_GroundPS:
 
-    def __init__(self, model_path_, ref_path_, image_topic_, depth_topic_, info_topic_, out_img_topic_, out_depth_topic_, out_score_topic_, out_info_topic_):
+    def __init__(self, model_path_, ref_path_, image_topic_, depth_topic_, depth_info_topic_, out_img_topic_, out_depth_topic_, out_score_topic_, out_info_topic_):
         self.get_cam_info = False
         self.imgOutput = rospy.Publisher(out_img_topic_, Image, queue_size=1)
         self.scoreOutput = rospy.Publisher(out_score_topic_, Image, queue_size=1)
@@ -17,7 +17,25 @@ class Img_GroundPS:
         self.infoOutput = rospy.Publisher(out_info_topic_, CameraInfo, queue_size=1)
         self.image_topic_ = image_topic_
         self.depth_topic_ = depth_topic_
-        self.info_topic_ = info_topic_
+        self.depth_info_topic_ = depth_info_topic_
+
+        rotation = np.array([[0.999994158744812, -0.001701555447652936, -0.0029583023861050606], [0.0016987272538244724, 0.9999980926513672, -0.0009582429775036871], [0.0029599270783364773, 0.0009532120893709362, 0.9999951720237732]])
+        translation = np.array([[-0.05916924029588699], [0.00016153734759427607], [0.000526325311511755]])
+        rgb_to_depth = np.linalg.inv(np.vstack((np.hstack((rotation,translation)),np.array([0,0,0,1])))) #rgb_to_depth
+        transformation = np.vstack((np.hstack((np.array([[1,0,0],[0,0.8660254,0.5],[0, -0.5,0.8660254]]),np.array([[0],[-1],[0]]))),np.array([0,0,0,1])))
+        rgb_to_depth = np.matmul(transformation,rgb_to_depth)
+
+        P_rgb = np.array([[381.8409423828125, 0.0, 315.6470031738281, 0.0], [0.0, 381.46221923828125, 243.59426879882812, 0.0], [0.0, 0.0, 1.0, 0.0]])
+        P_depth = np.array([[106.16276550292969, 0.0, 105.9286117553711, 0.0], [0.0, 106.16276550292969, 59.86717987060547, 0.0], [0.0, 0.0, 1.0, 0.0]])
+
+        H_depth = np.matmul(P_depth, transformation)
+        H_depth = np.delete(H_depth,2,1)
+
+        H_rgb = np.matmul(P_rgb, rgb_to_depth)
+        H_rgb = np.delete(H_rgb,2,1)
+
+        self.homography = np.matmul(H_depth,np.linalg.inv(H_rgb))
+
         # Load network to Inference Engine
         ie = IECore()
         net_ = ie.read_network(model=model_path_)
@@ -34,8 +52,8 @@ class Img_GroundPS:
     def run(self):
         image_sub = Subscriber(self.image_topic_, Image)
         depth_sub = Subscriber(self.depth_topic_, Image)
-        cam_info_sub = Subscriber(self.info_topic_, CameraInfo)
-        ts = ApproximateTimeSynchronizer([image_sub, depth_sub, cam_info_sub], queue_size=20, slop=0.1)
+        depth_info_sub = Subscriber(self.depth_info_topic_, CameraInfo)
+        ts = ApproximateTimeSynchronizer([image_sub, depth_sub, depth_info_sub], queue_size=20, slop=0.1)
         ts.registerCallback(self.img_callback)
         rospy.loginfo("Neural network online!")
 
@@ -46,7 +64,7 @@ class Img_GroundPS:
 
         try:
             bgr_img = cv2.resize(bridge.imgmsg_to_cv2(img_msg, 'bgr8'),(256,256))
-            depth_img = bridge.imgmsg_to_cv2(depth_msg, '16UC1')[:,39:199]
+            depth_img = bridge.imgmsg_to_cv2(depth_msg, '16UC1')# [:,39:199]
         except cv_bridge.CvBridgeError as e:
             rospy.logerr( 'image message to cv conversion failed :' )
             rospy.logerr( e )
@@ -59,10 +77,12 @@ class Img_GroundPS:
         HM = cv2.applyColorMap(scores, cv2.COLORMAP_JET)
         HM = (self.alpha*bgr_img + (1-self.alpha)*HM).astype(np.uint8)
 
-        depth_scores = cv2.resize(raw_score,(160,120))
-        # depth_img = np.where(np.any(cv2.resize(bgr_img,(160,120)) != 0, axis = 2), depth_img, 0) # show small obstacles
+        # depth_scores = cv2.warpPerspective(cv2.resize(raw_score,(640,480)),self.homography,(212,120))
+        depth_scores = cv2.warpAffine(cv2.resize(raw_score,(640,480)),self.homography[:2,:],(212,120))
+        
+        # depth_img = np.where(np.any(depth_scores != 0, axis = 2), depth_img, 0) # show small obstacles
         depth_img = np.where(depth_scores > 0.5, depth_img, 0)
-        score_img = np.array([np.zeros([120,160]),np.zeros([120,160]),depth_scores]).transpose(1, 2, 0)*255
+        score_img = np.array([np.zeros([120,212]),np.zeros([120,212]),depth_scores]).transpose(1, 2, 0)*255
 
         HM_out_msg = bridge.cv2_to_imgmsg(HM, 'bgr8')
         depth_out_msg = bridge.cv2_to_imgmsg(depth_img, '16UC1')
@@ -84,9 +104,11 @@ class Img_GroundPS:
 if __name__ == "__main__" :
     rospy.init_node('Img_GroundPS')
     try :
-        ir_16_path = '/home/upboard/OS_TR/openvino_models/FP16/ostr_mb3_large_ver2_shrinked.xml'
-        ref_path = '/home/upboard/OS_TR/test_images/ref_4.jpg'
-        IGSP = Img_GroundPS(ir_16_path, ref_path, "/image_in", "/depth_in", "info_in", "/ground_score/heatmap", "/ground_score/depth", "/ground_score/score", "/ground_score/camera_info")
+        # ir_16_path = '/home/upboard/OS_TR/openvino_models/FP16/ostr_mb3_large_ver2_shrinked/ostr_mb3_large_ver2_shrinked.xml'
+        # ir_16_path = '/home/upboard/OS_TR/openvino_models/FP16/ostr_mb3_large_ver4_perlin16_shrinked/ostr_mb3_large_ver4_perlin_shrinked.xml'
+        ir_16_path = '/home/upboard/OS_TR/openvino_models/FP16/ostr_mb3_large_ver4_perlin16_shrinked/ostr_mb3_large_ver4_perlin_shrinked.xml'
+        ref_path = '/home/upboard/OS_TR/test_images/ref.jpg'
+        IGSP = Img_GroundPS(ir_16_path, ref_path, "/image_in", "/depth_in", "depth_info_in", "/ground_score/heatmap", "/ground_score/depth", "/ground_score/score", "/ground_score/camera_info")
         IGSP.run()
         rospy.spin()
     except rospy.ROSInterruptException:
